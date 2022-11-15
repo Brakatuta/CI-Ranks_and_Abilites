6f8c8a1a265a49e2b19f0bbbd9ae925663e90f8e
Brakatuta <107749015+Brakatuta@users.noreply.github.com>
2022-11-15 15:00:17 +0100

    Update CI-Ranks.lua

--- CI-Ranks.lua
+++ CI-Ranks.lua
@@ -1,15 +1,15 @@
 local Ranks = {
     "$3850952944//Owner//Testing=true$",
    
-    "$1337426409//Admin$Testing=true",
-    "$993360326//Admin$Testing=true",
-    "$1310153058//Admin$Testing=true",
+    "$1337426409//Admin//Testing=true$",
+    "$993360326//Admin//Testing=true$",
+    "$1310153058//Admin//Testing=true$e",
    
-    "$44811788//Tester$Testing=true",
-    "$1853689359//Tester$Testing=true",
-    "$3988880902//Tester$Testing=true",
+    "$44811788//Tester//Testing=true$",
+    "$1853689359//Tester//Testing=true$",
+    "$3988880902//Tester//Testing=true$",
    
-    "$682615950//Contributor$Testing=false",
-    "$295419845//Contributor$Testing=false",
-    "$818547695//Contributor$Testing=false"
+    "$682615950//Contributor//Testing=false$",
+    "$295419845//Contributor//Testing=false$",
+    "$818547695//Contributor//Testing=false$"
 }
